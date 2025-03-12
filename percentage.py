3af3967d853983cf83c97f8409467f3ab419d78c
MadasuNagaSaiGreeshma <greeshmamadasu@gmail.com>
2025-03-12 22:26:18 +0530

    Create percentage.py

---
+++ percentage.py
@@ -0,0 +1,9 @@
+percent=int(input())
+if percent>90:
+    print("A grade")
+elif percent>=70 and percent<90:
+    print("B grade")
+elif percent>=50 and percent<70:
+    print("C grade")
+else:
+    print("Fail")
